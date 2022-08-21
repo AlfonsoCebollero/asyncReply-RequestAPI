cbadca2d516adaa443deebd76b050ca6b3336d79
acebollero <alfonso.cebollero@axians.es>
2022-08-21 02:47:24 +0200

    ADD not found workflow error

--- app/server/entities/errors.go
+++ app/server/entities/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ValidationError = errors.New("the received request is not valid")
 	BadWorkflow     = errors.New("workflow does not exist")
+	WfNotfoundError = errors.New("could not find workflow with the requested ID")
 )
