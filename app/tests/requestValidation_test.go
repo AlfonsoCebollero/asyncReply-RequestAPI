21667186474d7a2f4326be8c8a6997610b426cc0
acebollero <alfonso.cebollero@axians.es>
2022-08-21 21:34:37 +0200

    REFACTOR validation of creation request to match with use cases logic
    ADD request validation tests

---
+++ app/tests/requestValidation_test.go
@@ -0,0 +1,96 @@
+package tests
+
+import (
+	"OWTAssignment/app/config"
+	"OWTAssignment/app/useCases"
+	"fmt"
+	"os"
+	"path"
+	"testing"
+)
+
+func init() {
+	pwd, _ := os.Getwd()
+	root := path.Join(path.Dir(pwd), "../../")
+	os.Chdir(root)
+	fmt.Println("Changed directory to: " + root)
+	pwd, _ = os.Getwd()
+
+	config.AppConfig.LoadConfiguration()
+}
+
+func Test_workflowCreationRequestValidation(t *testing.T) {
+	type args struct {
+		taskListName string
+		duration     string
+		name         string
+		wfs          map[string]string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		want1   string
+		want2   int
+		wantErr bool
+	}{
+		{
+			name: "Successful validation",
+			args: args{
+				taskListName: "",
+				duration:     "35",
+				name:         "waitingwf",
+				wfs:          config.AppConfig.Cadence.Workflows,
+			},
+			want:    "asyncAPI",
+			want1:   "WaitingWorkflow",
+			want2:   35,
+			wantErr: false,
+		},
+		{
+			name: "Invalid duration",
+			args: args{
+				taskListName: "",
+				duration:     "25",
+				name:         "waitingwf",
+				wfs:          config.AppConfig.Cadence.Workflows,
+			},
+			want:    "",
+			want1:   "",
+			want2:   0,
+			wantErr: true,
+		},
+		{
+			name: "Non existing workflow",
+			args: args{
+				taskListName: "",
+				duration:     "35",
+				name:         "nonExisting",
+				wfs:          config.AppConfig.Cadence.Workflows,
+			},
+			want:    "",
+			want1:   "nonExisting",
+			want2:   0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, got2, err := useCases.WorkflowCreationRequestValidation(tt.args.taskListName, tt.args.duration, tt.args.name, tt.args.wfs)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("workflowCreationRequestValidation() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("workflowCreationRequestValidation() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("workflowCreationRequestValidation() got1 = %v, want %v", got1, tt.want1)
+			}
+			if got2 != tt.want2 {
+				t.Errorf("workflowCreationRequestValidation() got2 = %v, want %v", got2, tt.want2)
+			}
+
+		})
+	}
+}
---
+++ app/useCases/validation.go
@@ -0,0 +1,49 @@
+package useCases
+
+import (
+	"OWTAssignment/app/config"
+	"OWTAssignment/app/server/entities"
+	"OWTAssignment/app/worker/workflows"
+	"go.uber.org/zap"
+	"strconv"
+	"strings"
+)
+
+// WorkflowCreationRequestValidation validates if the request params are correct,
+// it also checks if a task list has been selected. Returns task list name, task duration and error.
+func WorkflowCreationRequestValidation(taskListName, duration, name string, wfs map[string]string) (string, string, int, error) {
+	logger := config.AppConfig.Logger
+	logger.Debug("Checking if a taskList has been given")
+	if taskListName == "" {
+		taskListName = workflows.TaskListName
+	}
+
+	logger.Info("Task list set.", zap.String("TaskListName", taskListName))
+
+	logger.Debug("Checking duration param.")
+	if duration == "" {
+		logger.Error("Duration has not been passed into the request")
+		return "", "", 0, entities.ValidationError
+	}
+	toIntDuration, err := strconv.Atoi(duration)
+	if err != nil {
+		logger.Error("Duration must be an integer", zap.String("Duration", duration))
+		return "", "", 0, entities.ValidationError
+	}
+
+	if toIntDuration < 30 {
+		logger.Error("Duration must be higher than or equal to 30", zap.Int("Duration", toIntDuration))
+		return "", "", 0, entities.ValidationError
+	}
+
+	logger.Debug("Checking name param.")
+
+	wfNameValue, exists := wfs[strings.ToLower(name)]
+	if !exists {
+		logger.Error(
+			"This workflow is not available or does not exist",
+			zap.Any("Available Options", wfs))
+		return "", name, 0, entities.BadWorkflow
+	}
+	return taskListName, wfNameValue, toIntDuration, nil
+}
